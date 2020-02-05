fa5f8d8d2521377e60c6ead2f1261f7ccdeb11e8
AladinBS <aladin.bensassi@gmail.com>
2020-02-05 23:45:48 +0100

    Update 100-shell_sort.c

--- 100-shell_sort.c
+++ 100-shell_sort.c
@@ -1,32 +1,43 @@
 #include "sort.h"
+
 /**
-* @array: Ordering
 * @size: array size.
+* @array: Ordering
 * shell_sort - sort array in asc order
 */
+
 void shell_sort(int *array, size_t size)
 {
-size_t intv = 0, i, j;
+size_t intv = 0, x1, x2;
 int x = 0;
+
 if (array == NULL || size < 2)
 return;
+
 while (intv <= (size / 3))
 {
 intv = intv * 3 + 1;
 }
+
 while (intv > 0)
 {
-for (i = intv; i < size; i++)
+	
+for (x1= intv; x1< size; x1++)
 {
-x = array[i];
-for (j = i; j >= intv && array[j - intv] > x;
-j -= intv)
+	
+	
+x = array[x1;
+for (x2 = x1; x2 >= intv && array[x2 - intv] > x;
+
+x2 -= intv)
 {
-array[j] = array[j - intv];
+array[x2] = array[x2 - intv];
 }
-array[j] = x;
+array[x2] = x;
 }
-print_array(array, size);
 intv = (intv - 1) / 3;
+print_array(array, size);
+
 }
+
 }
